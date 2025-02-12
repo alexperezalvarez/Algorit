196eb0bdaffb81632d0a50b1eaf31b0c15baeb97
Alexander <breynnerper18@gmail.com>
2025-02-11 19:21:53 -0500

    nuevos archivos

--- core/erp/tests.py
+++ core/erp/tests.py
@@ -1,31 +1,7 @@
 from config.wsgi import *
-from core.erp.models import Type
-def main():
-  # Listar
+from core.erp.models import *
 
-  #select * from Tabla
-  #query = Type.objects.all()
-  #rint(query)
+# LISTAR
 
-  #Insercion
-  #t = Type()
-  #t.name = 'Nuevo Tipo'
-  #t.save()
-
-  #edicion
-#  try:
- #     t = Type.objects.get(id=1)
-#    t.name = 'Tipo Editado'
-#      t.save()
-#      print(t.name)
-#  except Exception as e:
-#      print(e)
-
-
-  #Eliminacion
-  #t = Type.objects.get(id=1)
-  #t.delete()
-
- #obj = Type.objects.filter(name__icontains='terry')
- obj = Type.objects.filter(name__startswith='pre')
- print(obj)
+for i in Category.objects.filter():
+    print(i)
